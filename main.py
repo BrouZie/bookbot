acc5bbc9b33cd12129c38d3f381eb52b471f3303
BrouZie <sokrates.kjoelner@gmail.com>
2024-11-19 17:06:21 +0100

    D: returns dictionary with character count

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} found in the document")
+    print(get_character_count(text))
 
+def get_character_count(text):
+    lower_case = text.lower()
+    char_count = {}
+
+    for char in lower_case:
+        if char.isalpha():
+            char_count[char] = char_count.get(char, 0) + 1
+
+    return char_count
+    
 
 def get_num_words(text):
     words = text.split()
@@ -13,6 +24,5 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-        
         
 main()
