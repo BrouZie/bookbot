24e1a79fcc44016f6839491bb144008d0004f700
BrouZie <sokrates.kjoelner@gmail.com>
2024-11-19 17:43:41 +0100

    E: layout for printed stats

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} found in the document")
-    print(get_character_count(text))
+    character_count = get_character_count(text)
+    list_char_count = [{"character": char, "num": count} for char, count in character_count.items()]
+    list_char_count.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    for item in list_char_count:
+        print(f"The '{item['character']}' character was found {item['num']} times")
+    print("--- End report ---")
+    
+
+def sort_on(item):
+    return item["num"]
 
 def get_character_count(text):
     lower_case = text.lower()
